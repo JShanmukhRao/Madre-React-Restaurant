786417f24abde7512ffd70aaad5e1b588940d201
shubham <shubhamrao1233@gmail.com>
2019-07-04 13:32:53 +0530

    React Redux Forms Revisited.

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Breadcrumb, BreadcrumbItem,
             Button, Row, Col, Label } from 'reactstrap';
-import { Control, LocalForm, Errors } from 'react-redux-form';
+import { Control, Form, Errors, actions } from 'react-redux-form';
 import {Link} from 'react-router-dom';
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -19,11 +19,11 @@ class Contact extends Component {
     }
 
     
-    handleSubmit(values) {
+   handleSubmit(values) {
         console.log('Current State is: ' + JSON.stringify(values));
-              
-alert('Current State is: ' + JSON.stringify(values));
-    
+        alert('Current State is: ' + JSON.stringify(values));
+        this.props.resetFeedbackForm();
+        // event.preventDefault();
     }
   
   render() {
@@ -68,7 +68,8 @@ alert('Current State is: ' + JSON.stringify(values));
                       <h3>Send us your Feedback</h3>
                    </div>
                     <div className="col-12 col-md-9">
-<LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+  <Form model="feedback" onSubmit={(values) => this.handleSubmit(values)}>
+                          
                             <Row className="form-group">
                                 <Label htmlFor="firstname" md={2}>First Name</Label>
                                 <Col md={10}>
@@ -191,7 +192,7 @@ alert('Current State is: ' + JSON.stringify(values));
                                     </Button>
                                 </Col>
                             </Row>
-                        </LocalForm>
+                        </Form>
                         
 
                     </div>
